e8bffe7ad99d82bdb49b6ec28bc274c080b18330
Anvi <anvijana2007@gmail.com>
2020-08-25 23:20:27 +0000

    nothing

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("My name is Anvi and I live in Austin, TX.")
+print("Hello! Welcome to Anvi's Pizza Place What would you like to order? A - Cheese Pizza, B - Pepporoni Pizza, C - Veggie Pizza,   D - Supreme Pizza, .")
+order=input()
+if order == "A":
+  print("That will be $5.99. Have a great rest of your day!")
+elif order == "B":
+  print("That will be $9.99. Have a great rest of your day!")
+elif order == "C":
+  print("That will be $6.99. Have a great rest of your day!")
+elif order == "D":
+  print("That will be $11.99. Have a great rest of your day!")
+else:
+  print("I couldn't understand your answer. Please try again.")
